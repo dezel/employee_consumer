d43710f290553154dccdd0d7aa885e25a60b4366
Ezekiel Duku <ezedu01@gmail.com>
2023-05-30 15:12:08 +0000

    update various

--- src/App.js
+++ src/App.js
@@ -31,82 +31,113 @@ function App() {
 
   const [APIData, setAPIData] = useState([]);
   useEffect(() => {
-      axios.get ('https://emp-app-8gqr.onrender.com/get/')//('http://localhost:8000/get/')
-          .then((response) => {
-              setAPIData(response.data)
-              APIData.push({"first_name":"add", "last_name":"add"})
-              console.log(APIData)
-          })
+    axios.get('https://emp-app-8gqr.onrender.com/get/')//('http://localhost:8000/get/')
+      .then((response) => {
+        setAPIData(response.data.concat([{"id":0, "first_name": "", "last_name": " "}]))
+        // setAPIData([{ "first_name": "add", "last_name": "add" }])
+        console.log(APIData)
+      })
   }, [])
 
 
   const [first_name, setFirstName] = useState('');
   const [last_name, setLastName] = useState('');
   const [department, setDepartment] = useState('')
-  const [checkbox, setCheckbox] = useState(false);
+  const [checkbox, setCheckbox] = useState(false)
+  const [email, setEmail] = useState('')
+  const [active, setActive] = useState(false);
 
   function handleDelete(data) {
-      console.log(data)
-      axios.delete('http://localhost:8000/delete/'+String(data.id))
+    console.log(data)
+    axios.delete('http://localhost:8000/delete/' + String(data.id))
   }
 
-  function handleUpdate(data){
-      console.log(data)
-      axios.put('http://localhost:8000/update/', data)
+  function handleUpdate(data) {
+    // console.log(first_name, last_name, department)
+    
+    data.first_name = first_name?first_name:data.first_name
+    data.last_name = last_name?last_name:data.last_name
+    data.department = department?department:data.department
+    data.email = email?email:data.email
+
+    console.log(data)
+
+    axios.put('https://emp-app-8gqr.onrender.com/update/', data)
   }
-  
+
   const setData = (data) => {
-      console.log(data)
+    console.log(data)
   }
 
-  const handleActiveChange = e=>{
-      console.log(e.target)
-      // console.log(e.target.value)
-      // console.log(e.target.checked)
+
+  function handleAdd(data) {
+    console.log(first_name, last_name, department, email, active)
+    console.log(data)
+    axios.post('https://emp-app-8gqr.onrender.com/add/',
+      {
+        first_name,
+        last_name,
+        department,
+        email,
+        active
+      }
+    )
+  }
+
+  const handleActiveChange = e => {
+    console.log(e.target)
+    // console.log(e.target.value)
+    // console.log(e.target.checked)
   }
   return (
-      <div>
-          <Form>
-              <Table singleLine>
-                  <Table.Header>
-                      <Table.Row>
-                          {/* <Table.HeaderCell>id</Table.HeaderCell> */}
-                          <Table.HeaderCell>First Name</Table.HeaderCell>
-                          <Table.HeaderCell>Last Name</Table.HeaderCell>
-                          <Table.HeaderCell>Department</Table.HeaderCell>
-                          <Table.HeaderCell>Hire Date</Table.HeaderCell>
-                          <Table.HeaderCell>Email</Table.HeaderCell>
-                          {/* <Table.HeaderCell>Active</Table.HeaderCell> */}
-                          <Table.HeaderCell>Update</Table.HeaderCell>
-                          <Table.HeaderCell>Add</Table.HeaderCell>
-                          <Table.HeaderCell>Delete</Table.HeaderCell>
-                      </Table.Row>
-                  </Table.Header>
-                  <Table.Body>
-                      {APIData.map((data) => {
-                          return (
-                              <Table.Row>
-                                  {/* <Table.Cell>{data.id}</Table.Cell> */}
-                                  <Table.Cell><Input type='text' defaultValue={data.first_name} onChange={(e) => (data.first_name=e.target.value)}></Input></Table.Cell>
-                                  <Table.Cell><Input type='text' defaultValue={data.last_name} onChange={(e) => (data.last_name=e.target.value)}></Input></Table.Cell>
-                                  <Table.Cell><Input type = 'text' defaultValue ={data.department} onChange={(e) => (data.department=e.target.value)}/></Table.Cell>
-                                  <Table.Cell>{new Date(data.created).toDateString()}</Table.Cell>
-                                  <Table.Cell><Input type = 'text' defaultValue ={data.email} onChange={(e) => (data.email=e.target.value)}/></Table.Cell>
-                                  {/* <Table.Cell><input type='checkbox' value={data.active} onChange={(e)=>handleActiveChange(e)}/></Table.Cell> */}
-                                  <Table.Cell>
-                                      <Button onClick={(e) => handleUpdate(data)}>Update</Button>
-                                  </Table.Cell>
-                                  <Table.Cell>
-                                      <Button onClick={(e) => handleDelete(data)}>Delete</Button>
-                                  </Table.Cell>
-                              </Table.Row>
-                          )
-                      })
-                      }
-                  </Table.Body>
-              </Table>
-          </Form>
-      </div>
+    <div>
+      <Form>
+        <input type='text' ></input>
+        <Table singleLine>
+          <Table.Header>
+            <Table.Row>
+              {/* <Table.HeaderCell>id</Table.HeaderCell> */}
+              <Table.HeaderCell>First Name</Table.HeaderCell>
+              <Table.HeaderCell>Last Name</Table.HeaderCell>
+              <Table.HeaderCell>Department</Table.HeaderCell>
+              <Table.HeaderCell>Hire Date</Table.HeaderCell>
+              <Table.HeaderCell>Email</Table.HeaderCell>
+              {/* <Table.HeaderCell>Active</Table.HeaderCell> */}
+              <Table.HeaderCell>Update</Table.HeaderCell>
+              <Table.HeaderCell>Add</Table.HeaderCell>
+              <Table.HeaderCell>Delete</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {
+              APIData.map((data) => {
+                // console.log(APIData)
+                return (
+                  <Table.Row>
+                    {/* <Table.Cell>{data.id}</Table.Cell> */}
+                    <Table.Cell><Input type='text' defaultValue={data.first_name} onChange={(e) => (setFirstName(e.target.value))}></Input></Table.Cell>
+                    <Table.Cell><Input type='text' defaultValue={data.last_name} onChange={(e) => (setLastName(e.target.value))}></Input></Table.Cell>
+                    <Table.Cell><Input type='text' defaultValue={data.department} onChange={(e) => (setDepartment(e.target.value))} /></Table.Cell>
+                    <Table.Cell>{new Date(data.created).toDateString()}</Table.Cell>
+                    <Table.Cell><Input type='text' defaultValue={data.email} onChange={(e) => (setEmail(e.target.value))} /></Table.Cell>
+                    {/* <Table.Cell><input type='checkbox' value={data.active} onChange={(e)=>handleActiveChange(e)}/></Table.Cell> */}
+                    <Table.Cell>
+                      <Button onClick={(e) => handleUpdate(data)}>Update</Button>
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Button onClick={(e) => handleAdd(data)}>Add</Button>
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Button onClick={(e) => handleDelete(data)}>Delete</Button>
+                    </Table.Cell>
+                  </Table.Row>
+                )
+              })
+            }
+          </Table.Body>
+        </Table>
+      </Form>
+    </div>
   )
 }
 

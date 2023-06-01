16085a5294626ec68d128ca0cd7a6dc1856c9bda
Ezekiel Duku <ezedu01@gmail.com>
2023-06-01 07:54:54 +0000

    ‘update’

--- src/App.js
+++ src/App.js
@@ -52,16 +52,16 @@ function App() {
         setAPIData(response.data.concat([{ "applicant_number": "", "name": "", "phone": "" }]))
         // setAPIData([{ "applicant_id": 0, "name": "", "phone": "" }])
         // setAPIData([{ "first_name": "add", "last_name": "add" }])
-        console.log(APIData)
+        // console.log(APIData)
       }).catch((response) => {
-        console.log(response)
+        // console.log(response)
         setAPIData([{ "applicant_number": "", "name": "", "phone": "" }])
       })
   }, [])
 
   const [user, setUser] = useState(false)
 
-const [search, setSearch] = useState('')
+  const [search, setSearch] = useState('')
 
   const [applicant_number, setApplicantNumber] = useState('');
   const [name, setName] = useState('');
@@ -72,25 +72,25 @@ const [search, setSearch] = useState('')
   const [active, setActive] = useState(false);
 
   function handleDelete(data) {
-    console.log(data)
+    // console.log(data)
     axios.delete('https://job-application-wjg8.onrender.com/delete/' + String(data.id))
   }
 
 
-  function handleSearch(){
-    axios.get('https://job-application-wjg8.onrender.com/get/'+search)    //()'https://emp-app-8gqr.onrender.com/get/'
+  function handleSearch() {
+    axios.get('https://job-application-wjg8.onrender.com/get/' + search)    //()'https://emp-app-8gqr.onrender.com/get/'
       .then((response) => {
-        console.log(response.data)
-        const object_data = [] 
+        // console.log(response.data)
+        const object_data = []
         object_data.push(response.data)
 
         setAPIData(object_data.concat([{ "applicant_number": "", "name": "", "phone": "" }]))
         // setAPIData([{ "applicant_number": "", "name": "", "phone": "" }].push())
         // setAPIData([{ "applicant_id": 0, "name": "", "phone": "" }])
         // setAPIData([{ "first_name": "add", "last_name": "add" }])
-        console.log(APIData)
+        // console.log(APIData)
       }).catch((response) => {
-        console.log(response)
+        // console.log(response)
         setAPIData([{ "applicant_number": "", "name": "", "phone": "" }])
       })
   }
@@ -102,13 +102,13 @@ const [search, setSearch] = useState('')
     data.email = email ? email : data.email
     data.phone = phone ? phone : data.phone
 
-    console.log(data)
+    // console.log(data)
 
     axios.put('https://job-application-wjg8.onrender.com/update/', data)
   }
 
   const setData = (data) => {
-    console.log(data)
+    // console.log(data)
   }
 
   const [password, setPassword] = useState('')
@@ -122,45 +122,53 @@ const [search, setSearch] = useState('')
         username
       }
     ).then(response => {
-      console.log(response.status)
+      // console.log(response.status)
       if (response.status = 202) {
         setUser(true)
       }
     })
 
-    console.log(username, password)
+    // console.log(username, password)
   }
 
   function handleDownload(data) {
     // // console.log(data['cv'].replace('/music', ''))
-    try{console.log(data['cv'])
-    const bare_file = data['cv'].replace('\/media\/', '')
-    axios.post('https://job-application-wjg8.onrender.com/download/' + bare_file).then(response => {
-      console.log(response)
-      const url = window.URL.createObjectURL(new Blob([response.data]))
-      const link = document.createElement('a')
-      link.href = url
-      // link.setAttribute('download', 'file' + '.docx')
-      link.setAttribute('download', bare_file)
-      document.body.appendChild(link)
-      link.click()
+    try {
+      // console.log(data['cv'])
+      const bare_file = data['cv'].replace('\/media\/', '')
+
+      // axios.post('https://job-application-wjg8.onrender.com/download/' + bare_file, )
+      axios({
+        method: 'post',
+        url:'https://job-application-wjg8.onrender.com/download/'+ bare_file,
+        responseType: 'arraybuffer'
     })
-  }
-  catch{
-    console.log('client has no attachment')
-  }
+      .then(response => {
+        // console.log(response)
+        const url = window.URL.createObjectURL(new Blob([response.data]))
+        const link = document.createElement('a')
+        link.href = url
+        // link.setAttribute('download', 'file' + '.docx')
+        link.setAttribute('download', bare_file)
+        document.body.appendChild(link)
+        link.click()
+      })
+    }
+    catch {
+      console.log('client has no attachment')
+    }
 
   }
   function handleLogout() {
     axios.post(
-      'http://127.0.0.1:8000/logout/'
+      'https://job-application-wjg8.onrender.com/logout/'
     ).then(response => {
-      console.log(response)
+      // console.log(response)
       setUser(false)
       setPassword('')
       setUsername('')
     }).catch(response => {
-      console.log(response)
+      // console.log(response)
       setUser(false)
       setPassword('')
       setUsername('')
@@ -180,11 +188,9 @@ const [search, setSearch] = useState('')
     formData.append('email', email)
     formData.append('phone', phone)
     formData.append('cv', cvStart)
-    console.log(formData)
+    // console.log(formData)
 
-    for (var key of formData.entries()) {
-      console.log(key[0] + ', ' + key[1]);
-    }
+ 
     // axios.post('http://localhost:8000/add/',               //'https://emp-app-8gqr.onrender.com/add/',
     //   {
     //     body: formData
@@ -194,6 +200,8 @@ const [search, setSearch] = useState('')
     fetch('https://job-application-wjg8.onrender.com/add/', {
       method: 'POST',
       body: formData
+    }).catch(response=>{
+      console.log(response)
     })
   }
 
@@ -208,8 +216,8 @@ const [search, setSearch] = useState('')
     return (
       <div>
         <Form>
-          <Input onChange={(e)=>setSearch(e.target.value)} placeholder="search with id..." type='text' ></Input>
-          <Button onClick={()=>handleSearch()}><i className='ui search icon'></i></Button>
+          <Input onChange={(e) => setSearch(e.target.value)} placeholder="search with id..." type='text' ></Input>
+          <Button onClick={() => handleSearch()}><i className='ui search icon'></i></Button>
           <Button className="ui logout icon ui right floated clearing segment" onClick={() => handleLogout()}>Logout</Button>
           <Table singleLine>
             <Table.Header>
@@ -265,23 +273,58 @@ const [search, setSearch] = useState('')
     )
   } else {
     return (
-      <div>
-        <Form className="create-form">
-          <Form.Group>
-            <Form.Field>
-              <label>Username</label>
-              <Input onChange={(e) => setUsername(e.target.value)} placeholder='Username' />
-            </Form.Field>
-            <br>
-            </br>
-            <Form.Field>
-              <label>Password</label>
-              <Input placeholder='Password' onChange={(e) => setPassword(e.target.value)} type='password' />
-            </Form.Field>
-            <Button type='submit' onClick={() => handleLogin()}>Login</Button>
-          </Form.Group>
-        </Form>
+      // <div>
+      //   <Form className="create-form">
+      //     <Form.Group>
+      //       <Form.Field>
+      //         <label>Username</label>
+      //         <Input onChange={(e) => setUsername(e.target.value)} placeholder='Username' />
+      //       </Form.Field>
+      //       <br/>
+      //         <Form.Field>
+      //           <label>Password</label>
+      //           <Input placeholder='Password' onChange={(e) => setPassword(e.target.value)} type='password' />
+      //         </Form.Field>
+      //         <Form.Field>
+      //           <Button type='submit' onClick={() => handleLogin()}>Login</Button>
+      //         </Form.Field>
+      //     </Form.Group>
+      //   </Form>
 
+      // </div>
+
+      <div className="page-login">
+        <div className="ui centered grid container">
+          <div className="nine wide column">
+            <div className="ui icon warning message">
+              <i className="lock icon"></i>
+              <div className="content">
+                <div className="header">
+                  Login
+                </div>
+               
+              </div>
+            </div>
+            <div className="ui fluid card">
+              <div className="content">
+                <Form className="ui form" method="POST">
+                  <div className="field">
+                    <label>User</label>
+                    <Input onChange={(e) => setUsername(e.target.value)} type="text" placeholder="User" />
+                  </div>
+                  <div className="field">
+                    <label>Password</label>
+                    <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
+                  </div>
+                  <Button type='submit' onClick={() => handleLogin()} className="ui primary labeled icon button">
+                    <i className="unlock alternate icon"></i>
+                    Login
+                  </Button>
+                </Form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
